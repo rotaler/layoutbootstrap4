31ce40ca59bb88236cc0ef98fc95c05472ecf309
rotaler <pkopchuk@yandex.ru>
2019-12-13 06:47:16 +0300

    create profile

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('.button[filter="wd"]').click(function(){
+        if($(this).attr('val')=='off') {
+        $('.button[filter]').attr('val', 'off');
+        if($(this).attr('val')=='on');
+        $('.filter > div').hide(300);
+        $('.filter > div[filter="wd"]').show(300);
+        }
+        
+    })
+    $('.button[filter="ud"]').click(function(){
+        if($(this).attr('val')=='off') {
+        $('.button[filter]').attr('val', 'off');
+        if($(this).attr('val')=='on');
+        $('.filter > div').hide(300);
+        $('.filter > div[filter="ud"]').show(300);
+        }
+        
+    })
+    $('.button[filter="moc"]').click(function(){
+        if($(this).attr('val')=='off') {
+        $('.button[filter]').attr('val', 'off');
+        if($(this).attr('val')=='on');
+        $('.filter > div').hide(300);
+        $('.filter > div[filter="moc"]').show(300);
+        }
+        
+    })
+    $('.button[filter="all"]').click(function(){
+        if($(this).attr('val')=='off') {
+        $('.button[filter]').attr('val', 'off');
+        if($(this).attr('val')=='on');
+        $('.filter > div').show(300);
+        }
+        
+    })
+    });
+    
+
+
+
+
